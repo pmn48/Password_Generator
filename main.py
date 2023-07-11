2c7eccf1afd8355c3ca96305e59527b47e3ec85e
pmn48 <138720690+pmn48@users.noreply.github.com>
2023-07-11 02:54:59 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from passgen import PassWord
+import json as js
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -14,16 +15,56 @@ def save_pw():
     web_entry = web_box.get()
     user_entry = user_box.get()
     pw_entry = pw_box.get()
+    new_data = {
+        web_entry: {
+            "Email": user_entry, "Password": pw_entry
+        }
+    }
     if web_entry == "" or pw_entry == "" or user_entry == "":
-        messagebox.showerror(title="ERROR", message="Incomplete section(s)")
+        messagebox.showerror(title="Error", message="Incomplete section(s)")
     else:
         is_ok = messagebox.askokcancel(title=web_entry,
                                        message=f"These are the details entered: \nEmail: {user_entry} \nPassword: {pw_entry} \nOK to save?")
         if is_ok:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{web_entry} | {user_entry} | {pw_entry}\n")
+            try:
+                with open("data.json", mode="r") as data_file:
+                    # reading old data
+                    data = js.load(data_file)
+                    # update old data with new data
+                    data.update(new_data)
+            except FileNotFoundError:
+                # Create a new data file and save the pw
+                with open("data.json", mode="w") as data_file:
+                    js.dump(new_data, data_file, indent=4)
+            else:
+                with open("data.json", mode="w") as data_file:
+                    # Save the updated data
+                    js.dump(data, data_file, indent=4)
+            finally:
                 web_box.delete(0, END)
                 pw_box.delete(0, END)
+
+
+# ------------------------------SEARCH INFO----------------------------------#
+def search_info():
+    web_entry = web_box.get()
+    if len(web_entry) == 0:
+        messagebox.showerror(title="Error", message="Incomplete Website Section")
+    else:
+        try:
+            with open("data.json", mode="r") as data_file:
+                content = js.load(data_file)
+                line = ""
+                for key, value in content[web_entry].items():
+                    line += f"{key}: {value}\n"
+        except KeyError:
+            # The website has not been saved in the json file
+            messagebox.showerror("Error", f"No data for {web_entry} exists")
+        except FileNotFoundError:
+            # There is no json file yet
+            messagebox.showerror("Error", "No Data File found\nCreate the first entry by adding all sections")
+        else:
+            messagebox.askquestion(title=web_entry, message=line)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,19 +90,23 @@ password.grid(column=0, row=3)
 gen_pw = Button(text="Generate Password", highlightthickness=0, command=gen_pw)
 gen_pw.grid(column=2, row=3)
 
-add_b = Button(text="Add", width=36, highlightthickness=0, command=save_pw)
-# add_b.grid(column=1, row=4, columnspan=2)
-add_b.place(x=145, y=270)
+add_b = Button(text="Add", width=45, highlightthickness=0, command=save_pw)
+add_b.grid(column=1, row=4, columnspan=3)
+# add_b.place(x=145, y=270)
+
+search_b = Button(text="Search", width=14, highlightthickness=0, command=search_info)
+search_b.grid(column=2, row=1)
+
 # ENTRIES
 web_box = Entry(width=35)
-web_box.grid(column=1, row=1, columnspan=2)
+web_box.grid(column=1, row=1)
 web_box.focus()
 
-user_box = Entry(width=35)
-user_box.grid(column=1, row=2, columnspan=2)
+user_box = Entry(width=53)
+user_box.grid(column=1, row=2, columnspan=3)
 user_box.insert(END, "myemail@email.com")  # pre-populated email
 
-pw_box = Entry(width=24)
-pw_box.place(x=145, y=248)
-# pw_box.grid(column=1, row=3)
+pw_box = Entry(width=35)
+# pw_box.place(x=145, y=248)
+pw_box.grid(column=1, row=3)
 screen.mainloop()
